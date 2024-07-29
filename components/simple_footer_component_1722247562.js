cd13a5a76635103efe5e6793d7f2b1e99cf439bf
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-29 10:06:11 +0000

    Placeholder text replacement

--- components/simple_footer_component_1722247562.js
+++ components/simple_footer_component_1722247562.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1722247562", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            DabbaMaker: Craft Your Culinary Delights
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Experience the joy of homemade meals with DabbaMaker. Our app guides you through creating delicious and nutritious dabbas, perfect for busy days. Join our community of food enthusiasts and make mealtime a hassle-free delight.
                     </div>
                 </div>
             </div>
